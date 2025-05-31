d7aae893a0077d9f9a6f0282758697b4a7ec8b43
Pattisier <Principursa@protonmail.com>
2025-05-31 22:59:27 +0200

    tests working with multiple assets

--- test/MainnetETFVaultManager.t.sol
+++ test/MainnetETFVaultManager.t.sol
@@ -12,8 +12,8 @@ contract MainnetETFVaultManagerTest is Test {
     address public factory = 0x420DD381b31aEf6683db6B902084cB0FFECe40Da;
     address public aeroRouter = 0xcF77a3Ba9A5CA399B7c97c74d54e5b1Beb874E43;
     address wbtc = 0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf; // Base WBTC
-    address shib = 0x83A8b791B77c5616f8a3CFdf830B0fF1aDb13a4F; // Base SHIB  
-    address wif = 0xB0fFa8000886e57F86dd5264b9582b2Ad87b2b91; // Base WIF
+    address weth = 0x4200000000000000000000000000000000000006; // Base SHIB  
+    address doge = 0x67f0870BB897F5E1c369976b4A2962d527B9562c; // Base WIF
     
 
     function setUp() public {
@@ -31,18 +31,20 @@ contract MainnetETFVaultManagerTest is Test {
       USDC.approve(address(manager),tokenAmt);
 
       // Test with just WBTC which is more likely to have liquidity
-      address[] memory tokens = new address[] (1);
+      address[] memory tokens = new address[] (3);
       tokens[0] = wbtc;
-      // tokens[1] = shib;
-      // tokens[2] = wif;
-      uint256[] memory weights = new uint256[] (1);
-      weights[0] = 10000; // 100%
-      // weights[1] = 3000;
-      // weights[2] = 2000;
+      tokens[1] = weth;
+      tokens[2] = doge;
+      uint256[] memory weights = new uint256[] (3);
+      weights[0] = 5000; // 100%
+      weights[1] = 3000;
+      weights[2] = 2000;
 
       manager.deposit(tokenAmt,tokens,weights);
       vm.stopPrank();
       assertNotEq(0, IERC20(wbtc).balanceOf(address(manager)));
+      assertNotEq(0, IERC20(weth).balanceOf(address(manager)));
+      assertNotEq(0, IERC20(doge).balanceOf(address(manager)));
 
       }
 
